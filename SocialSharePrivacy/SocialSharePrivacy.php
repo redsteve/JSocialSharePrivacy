c9b41a29688ef21209195e19ff1b4c379b556741
redsteve <redsteve@gmx.de>
2011-10-09 22:15:07 +0200

    Implementation continued...

--- SocialSharePrivacy/SocialSharePrivacy.php
+++ SocialSharePrivacy/SocialSharePrivacy.php
@@ -15,9 +15,13 @@ define('SOCIAL_SHARE_PRIVACY_BASE_URL', JURI::base() . SOCIAL_SHARE_PRIVACY_RELA
 
 final class plgContentSocialSharePrivacy extends JPlugin {
 
-	const divElementToSubstitute = "\n<div id=\"socialshareprivacy\"></div>\n";
+	const facebookServiceName = "facebook";
+	const twitterServiceName = "twitter";
+	const googleServiceName = "gplus";
+	const socialSharePrivacyDivElement = "\n<div id=\"socialshareprivacy\"></div>\n";
 	const heisePluginFolderName = "socialshareprivacy";
-	const defaultCssPathAndFilename = "socialshareprivacy\socialshareprivacy.css";
+	const defaultDummyImagePath = "socialshareprivacy/images/";
+	const defaultCssPathAndFilename = "socialshareprivacy/socialshareprivacy.css";
 	const socialSharePrivacyJScript = "jquery.socialshareprivacy.js";
 	const socialSharePrivacyMinimizedJScript = "jquery.socialshareprivacy.min.js";
 
@@ -31,31 +35,42 @@ final class plgContentSocialSharePrivacy extends JPlugin {
 		
 		if ($this->isAdministrationArea($application))
 			return;
-			
+		
+		$currentOption = JRequest::getCmd("option");
+		if (($currentOption != "com_content")
+			OR !isset($article)
+			OR empty($article->id)
+			OR !isset($this->params)) {
+            return;            
+        }
+		
 		$document = &JFactory::getDocument();
 		if (! $this->isHtmlDocument($document))
 			return;
-			
+
 		if (! $this->isArticle())
 			return;
-			
+
 		$this->addCustomTagsToHeader($document);
-		
-		$position = $this->params->get('position');
-		
-		switch ($position) {
-			case 1 :
-				$article->text = self::divElementToSubstitute . $article->text;
-				break;
-				
-			case 2 :
-				$article->text = $article->text . self::divElementToSubstitute;
-				break;
-		}
+		$this->insertSocialshareprivacyDivElementIntoArticle($article);
 		
 		return true;
     }
 
+	private function insertSocialshareprivacyDivElementIntoArticle($article) {
+		$position = $this->params->get('position');
+		
+		switch ($position) {
+			case 1 :
+				$article->text = self::socialSharePrivacyDivElement . $article->text;
+				break;
+				
+			case 2 :
+				$article->text = $article->text . self::socialSharePrivacyDivElement;
+				break;
+		}
+	}
+	
     private function isAdministrationArea(&$application) {
 		return ($application->isAdmin());
 	}
@@ -75,21 +90,46 @@ final class plgContentSocialSharePrivacy extends JPlugin {
 		return (! (empty($jQueryUrl)));
 	}
 
+	private function isParameterFacebookDummyImageNotEmpty() {
+		$facebookDummyImage = $this->params->get('facebookDummyImage');
+		return (! (empty($facebookDummyImage)));
+	}
+
+	private function isParameterTwitterDummyImageNotEmpty() {
+		$twitterDummyImage = $this->params->get('twitterDummyImage');
+		return (! (empty($twitterDummyImage)));
+	}
+
+	private function isParameterGoogleDummyImageNotEmpty() {
+		$googleDummyImage = $this->params->get('googleDummyImage');
+		return (! (empty($googleDummyImage)));
+	}
+
 	private function isParameterFacebookReferrerTrackNotEmpty()	{
 		$facebookReferrerTrack = $this->params->get('facebookReferrerTrack');
 		return (! (empty($facebookReferrerTrack)));
 	}
-	
-	private function isParameterTwitterReferrerTrackNotEmpty()	{
+
+	private function isParameterTwitterReferrerTrackNotEmpty() {
 		$twitterReferrerTrack = $this->params->get('twitterReferrerTrack');
 		return (! (empty($twitterReferrerTrack)));
 	}
-	
-	private function isParameterGoogleReferrerTrackNotEmpty()	{
+
+	private function isParameterGoogleReferrerTrackNotEmpty() {
 		$googleReferrerTrack = $this->params->get('googleReferrerTrack');
 		return (! (empty($googleReferrerTrack)));
 	}
-	
+
+	private function isParameterCssPathNotEmpty() {
+		$cssPath = $this->params->get('cssPath');
+		return (! (empty($cssPath)));
+	}
+
+	private function isParameterCookieDomainNotEmpty() {
+		$cookieDomain = $this->params->get('cookieDomain');
+		return (! (empty($cookieDomain)));
+	}
+
     private function addCustomTagsToHeader(&$document) {
 		if ($this->isParameterJQueryUrlNotEmpty()) {
 			$this->addJQueryUrlToHeader($document);
@@ -117,7 +157,7 @@ final class plgContentSocialSharePrivacy extends JPlugin {
 	private function addOptionsJScriptToHeader(&$document) {
 		$heisePluginConfig = $this->getHeisePluginConfig();
 		$javascript = $this->getHeisePluginConfigJavaScript($heisePluginConfig);
-		/** $document->addScriptDeclaration($javascript); */
+		$document->addCustomTag($javascript);
 	}
 
 	private function getHeisePluginUrl() {
@@ -127,7 +167,7 @@ final class plgContentSocialSharePrivacy extends JPlugin {
 		
 		return $heisePluginUrl;
 	}
-	
+
 	private function getHeisePluginConfig() {
 		$config = "\n    services : {";
 		
@@ -137,18 +177,26 @@ final class plgContentSocialSharePrivacy extends JPlugin {
 		
 		$config .= "\n    },";
 		
-		$this->appendCommonConfig($config);
+		$this->appendCommonConfigSection($config);
 		
 		return $config;
 	}
 
 	private function appendFacebookConfigSection(&$config) {
-		$this->openSocialServiceConfigSection($config, "facebook");
+		$this->openSocialServiceConfigSection($config, self::facebookServiceName);
 		
 		$displayFacebook = $this->params->get('displayFacebook');
 		$this->appendEnableServiceConfigFragment($config, $displayFacebook);
 		
 		if ($displayFacebook) {
+			$facebookDummyImage = "";
+			if ($this->isParameterFacebookDummyImageNotEmpty()) {
+				$facebookDummyImage = $this->params->get('facebookDummyImage');
+			} else {
+				$facebookDummyImage = $this->getFacebookDefaultDummyImagePathAndFilename();
+			}
+			$this->appendDummyImageOptionFragment($config, $facebookDummyImage);
+			
 			$facebookPermaOption = $this->params->get('facebookPermaOption');
 			$this->appendEnablePermaOptionFragment($config, $facebookPermaOption);
 			
@@ -162,12 +210,20 @@ final class plgContentSocialSharePrivacy extends JPlugin {
 	}
 
 	private function appendTwitterConfigSection(&$config) {
-		$this->openSocialServiceConfigSection($config, "twitter");
+		$this->openSocialServiceConfigSection($config, self::twitterServiceName);
 		
 		$displayTwitter = $this->params->get('displayTwitter');
 		$this->appendEnableServiceConfigFragment($config, $displayTwitter);
 		
 		if ($displayTwitter) {
+			$twitterDummyImage = "";
+			if ($this->isParameterTwitterDummyImageNotEmpty()) {
+				$twitterDummyImage = $this->params->get('twitterDummyImage');
+			} else {
+				$twitterDummyImage = $this->getTwitterDefaultDummyImagePathAndFilename();
+			}
+			$this->appendDummyImageOptionFragment($config, $twitterDummyImage);
+			
 			$twitterPermaOption = $this->params->get('twitterPermaOption');
 			$this->appendEnablePermaOptionFragment($config, $twitterPermaOption);
 			
@@ -181,18 +237,27 @@ final class plgContentSocialSharePrivacy extends JPlugin {
 	}
 
 	private function appendGooglePlusConfigSection(&$config) {
-		$this->openSocialServiceConfigSection($config, "gplus");
+		$this->openSocialServiceConfigSection($config, self::googleServiceName);
 		
 		$displayGooglePlus = $this->params->get('displayGooglePlus');
 		$this->appendEnableServiceConfigFragment($config, $displayGooglePlus);
 		
 		if ($displayGooglePlus) {
+			$googleDummyImage = "";
+			if ($this->isParameterGoogleDummyImageNotEmpty()) {
+				$googleDummyImage = $this->params->get('googleDummyImage');
+			} else {
+				$googleDummyImage = $this->getGoogleDefaultDummyImagePathAndFilename();
+			}
+			$this->appendDummyImageOptionFragment($config, $googleDummyImage);
+			
 			$googlePermaOption = $this->params->get('googlePermaOption');
 			$this->appendEnablePermaOptionFragment($config, $googlePermaOption);
 			
 			if ($this->isParameterGoogleReferrerTrackNotEmpty()) {
 				$googleReferrerTrack = $this->params->get('googleReferrerTrack');
 				$this->appendReferrerTrackOptionFragment($config, $googleReferrerTrack);
+			}
 		}
 		
 		$this->closeSocialServiceConfigSection($config);
@@ -209,17 +274,47 @@ final class plgContentSocialSharePrivacy extends JPlugin {
 	}
 	
 	private function appendEnableServiceConfigFragment(&$config, $enableFlag) {
-		$config = "\n        'status' : ";
+		$config .= "\n        'status' : ";
 		$this->appendBooleanFlag(&$config, $enableFlag);
 	}
 
 	private function appendEnablePermaOptionFragment(&$config, $enableFlag) {
-		$config = "\n        'perma_option' : ";
+		$config .= "\n        'perma_option' : ";
 		$this->appendBooleanFlag(&$config, $enableFlag);
 	}
 
+	private function appendDummyImageOptionFragment(&$config, $dummyImagePathAndFilename) {
+		$config .= "\n        'dummy_img' : '";
+		$config .= $dummyImagePathAndFilename;
+		$config .= "',";
+	}
+
+	private function getFacebookDefaultDummyImagePathAndFilename() {
+		return $this->getDefaultDummyImagePathAndFilename(self::facebookServiceName);
+	}
+
+	private function getTwitterDefaultDummyImagePathAndFilename() {
+		return $this->getDefaultDummyImagePathAndFilename(self::twitterServiceName);
+	}
+
+	private function getGoogleDefaultDummyImagePathAndFilename() {
+		return $this->getDefaultDummyImagePathAndFilename(self::googleServiceName);
+	}
+
+	private function getDefaultDummyImagePathAndFilename($serviceName) {
+		$dummyImagePathAndFilename  = SOCIAL_SHARE_PRIVACY_BASE_URL;
+		$dummyImagePathAndFilename .= self::heisePluginFolderName;
+		$dummyImagePathAndFilename .= "/";
+		$dummyImagePathAndFilename .= self::defaultDummyImagePath;
+		$dummyImagePathAndFilename .= "dummy_";
+		$dummyImagePathAndFilename .= $serviceName;
+		$dummyImagePathAndFilename .= ".png";
+		
+		return $dummyImagePathAndFilename;
+	}
+
 	private function appendReferrerTrackOptionFragment(&$config, $referrerTrack) {
-		$config  = "\n        'referrer_track' : '";
+		$config .= "\n        'referrer_track' : '";
 		$config .= $referrerTrack;
 		$config .= "',";
 	}
@@ -232,16 +327,53 @@ final class plgContentSocialSharePrivacy extends JPlugin {
 		}
 	}
 
-	private function appendCommonConfig(&$config) {
-		$config .= "\n      'css_path' : 'socialshareprivacy/socialshareprivacy.css'";
+	private function appendCommonConfigSection(&$config) {
+		$cssPathAndFilename = "";
+		if ($this->isParameterCssPathNotEmpty()) {
+			$cssPathAndFilename = $this->params->get('cssPath');
+		} else {
+			$cssPathAndFilename = $this->getDefaultCssPathAndFilename();
+		}
+		$this->appendCssPathOptionFragment($config, $cssPathAndFilename);
+		
+		if ($this->isParameterCookieDomainNotEmpty()) {
+			$cookieDomain = $this->params->get('cookieDomain');
+			$this->appendCookieDomainOptionFragment($config, $cookieDomain);
+		}
+	}
+
+	private function getDefaultCssPathAndFilename()
+	{
+		$defaultCssPathAndFilename  = SOCIAL_SHARE_PRIVACY_BASE_URL;
+		$defaultCssPathAndFilename .= self::heisePluginFolderName;
+		$defaultCssPathAndFilename .= "/";
+		$defaultCssPathAndFilename .= self::defaultCssPathAndFilename;
+		
+		return $defaultCssPathAndFilename ;
+	}
+
+	private function appendCssPathOptionFragment(&$config, $cssPathAndFilename) {
+
+		$config .= "\n      'css_path' : '";
+		$config .= $cssPathAndFilename;
+		$config .= "',";
+	}
+
+	private function appendCookieDomainOptionFragment(&$config, $cookieDomain) {
+
+		$config .= "\n      'cookie_domain' : '";
+		$config .= $cookieDomain;
+		$config .= "',";
 	}
 
 	private function getHeisePluginConfigJavaScript($heisePluginConfig) {
-		$javascript  = "    jQuery(document).ready(function($) {\n";
+		$javascript  = "  <script type=\"text/javascript\">\n";
+		$javascript .= "    jQuery(document).ready(function($) {\n";
 		$javascript .= "      if ($('#socialshareprivacy').length > 0) {\n";
-		$javascript .= "        $('#socialshareprivacy').socialSharePrivacy(" . $heisePluginConfig . ");\n";
-		$javascript .= "      }\n" ;
-		$javascript .= "    });\n";
+		$javascript .= "        $('#socialshareprivacy').socialSharePrivacy({" . $heisePluginConfig . "})\n";
+		$javascript .= "      }});\n";
+		$javascript .= "  </script>\n";
+		
 		
 		return $javascript;
 	}
